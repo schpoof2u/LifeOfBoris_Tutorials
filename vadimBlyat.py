eed033cced1759b1a82d337a9ccf09a9ed52475a
schpoof2u <62222006+schpoof2u@users.noreply.github.com>
2021-02-13 21:25:23 -0500

    Python Tutorial
    followed boris tutorial vid and set up vscode for python development

---
+++ vadimBlyat.py
@@ -0,0 +1,16 @@
+print("Hello, Vadim")
+print("What is your name blin?")
+user_name=input()
+if user_name=="Vadim":
+    while 3>2: 
+        count=0
+        if(count==5):
+            print("Vadim Blyat")
+            break
+        print("Vadim Blyat")
+        count=count+1
+elif user_name=="Boris":
+    print("Boris wifi password is cheeki breeki")
+else:
+    print("Hello, "+user_name+" have a nice day blin")
+input("press Enter key to exit")
